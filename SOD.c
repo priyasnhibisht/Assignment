ea2a061e190744a68f1801fa811ea194453b6150
priyasnhibisht <142192511+priyasnhibisht@users.noreply.github.com>
2024-02-21 08:51:22 +0530

    Update

---
+++ SOD.c
@@ -0,0 +1,28 @@
+/* NAME : PRIYANSHI BISHT 
+   SECTION : A
+   ROLL NO. : 47
+WAP in C to create a recursive function to calculate the sum of digits of a given number.
+*/
+
+#include <stdio.h>
+
+int sumOfDigits(int num) {
+   
+    if (num == 0) {
+        return 0;
+    }
+
+    return (number %10)+sumOfDigits(number / 10);
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int sum = sumOfDigits(num);
+    printf("Sum of digits: %d\n", sum);
+
+    return 0;
+}
